e2a659e08eaa32136f1a587b104f4d76c8ed5da9
Sadek Baroudi <sadekbaroudi@gmail.com>
2021-05-19 23:15:26 -0700

    initial commit

---
+++ keyboards.js
@@ -0,0 +1,30 @@
+module.exports = {
+    euclid36: {
+        vendorId: 65278,
+        productId: 23384,
+    	cpuLeds: [0],
+    	diskLeds: [1],
+    	memoryLeds: [2],
+    	stockLeds: [3,4,5,6,7,8,9,10,11,12]
+    },
+    enigma36: {
+        vendorId: 65278,
+        productId: 23499,
+    	cpuLeds: [],
+    	diskLeds: [],
+    	memoryLeds: [],
+    	stockLeds: [1,2,3,4,5,6,7,8,9,10,11]
+    },
+    reviung41: {
+    	cpuLeds: [],
+    	diskLeds: [],
+    	memoryLeds: [0],
+    	stockLeds: [1,2,3,4,5,6,7,8,9,10]
+    },
+    reviung39: {
+    	cpuLeds: [],
+    	diskLeds: [],
+    	memoryLeds: [0],
+    	stockLeds: [1,2,3,4,5,6,7,8,9,10]
+    },
+};
