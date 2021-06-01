b4ec1e1927c5678d69137d31ae275fc712dd42df
Sadek Baroudi <sadekbaroudi@gmail.com>
2021-06-01 01:12:09 -0700

    added new handwired enimga36

--- keyboards.js
+++ keyboards.js
@@ -14,7 +14,7 @@ module.exports = {
     	// memoryLeds: [1],
     	// stockLeds: [2]//,3,4,5,6,7,8,9,10,11,12] // commented out to leave the underglow free, just use the top three leds
     },
-    enigma36: {
+    enigma36_1: {
         vendorId: 65278,
         productId: 23499,
         usagePage: 65376,
@@ -23,6 +23,16 @@ module.exports = {
     	diskLeds: [],
     	memoryLeds: [4,5,6,7],
     	stockLeds: [8,9,10,11]
+    },
+    enigma36_2: {
+        vendorId: 65278,
+        productId: 23499,
+        usagePage: 65376,
+        numberOfLeds: 12,
+        cpuLeds: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14],
+        diskLeds: [],
+        memoryLeds: [15,16,17,18,19,20],
+        stockLeds: [21,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]
     },
     reviung41: {
         numberOfLeds: 11,
